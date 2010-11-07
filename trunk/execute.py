da08b4416cd33e78c2774ec92593b70a0ee5c71c
ondrovic <ondrovic@316976be-1fe1-584a-d250-782a3570e90c>
2010-11-07 22:15:49 +0000

    Fixed bug

--- trunk/execute.py
+++ trunk/execute.py
@@ -1,8 +1,28 @@
 import os
 import sys
-import commands
+from PyQt4 import QtGui,QtCore
+from main_window import *
 
-variable = sys.argv[0]
-direc = variable.replace('execute.py',"")
-os.system('cd %s \n python fern.py'%(direc))
+class mainwindow(QtGui.QDialog,Ui_Dialog):
+    def __init__(self):
+        QtGui.QDialog.__init__(self)
+
+        #
+        # Checks privilegde level - for aircrack-ng suite needs those
+        #
+        if os.getenv('LOGNAME','none').lower() == 'root':
+            variable = sys.argv[0]
+            direc = variable.replace('execute.py',"")
+            os.system('cd %s \n python fern.py'%(direc))
+        else:
+            QtGui.QMessageBox.warning(self,"Insufficient Priviledge","Aircrack and other dependencies need root priviledge to function, Please run application as root")
+            sys.exit()
+        
+   
+
+app = QtGui.QApplication(sys.argv)
+run = mainwindow()
+run.show()
+app.exec_()
+
 
